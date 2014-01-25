a36a64ffa604c60af57676dda72899cdecfb9a2b
Jesse Morgan <morgajel@gmail.com>
2014-01-24 21:59:28 -0500

    adding package for trees

--- gimp-brushes/package.sh
+++ gimp-brushes/package.sh
@@ -3,3 +3,4 @@
 zip MedinaBuildings.zip     MedinaBuildings.gih     Thank\ You.txt
 zip UniqueBuildings.zip     UniqueBuildings.gih     Thank\ You.txt
 zip GenericBuildings.zip    GenericBuildings.gih    Thank\ You.txt
+zip Trees.zip               Trees.gih               Thank\ You.txt
