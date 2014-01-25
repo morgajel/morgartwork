61101a3e70eb16529435e1fdd9d65b4b515c603c
Jesse Morgan <morgajel@gmail.com>
2014-01-24 21:58:33 -0500

    fun with brushes- tree time

--- gimp-brushes/package.sh
+++ gimp-brushes/package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-zip UniqueBuildings.zip UniqueBuildings.gih  Thank\ You.txt
-zip GenericHouses.zip   GenericHouses.gih    Thank\ You.txt
+zip MedinaBuildings.zip     MedinaBuildings.gih     Thank\ You.txt
+zip UniqueBuildings.zip     UniqueBuildings.gih     Thank\ You.txt
+zip GenericBuildings.zip    GenericBuildings.gih    Thank\ You.txt
