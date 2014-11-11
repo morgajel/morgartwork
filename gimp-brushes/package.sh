30f1fcaf6328b1fcfb1cde72a96dfc97813f029d
Jesse Morgan <morgajel@gmail.com>
2014-11-10 23:21:31 -0500

    sanity save

--- gimp-brushes/package.sh
+++ gimp-brushes/package.sh
@@ -4,3 +4,6 @@ zip MedinaBuildings.zip     MedinaBuildings.gih     Thank\ You.txt
 zip UniqueBuildings.zip     UniqueBuildings.gih     Thank\ You.txt
 zip GenericBuildings.zip    GenericBuildings.gih    Thank\ You.txt
 zip Trees.zip               Trees.gih               Thank\ You.txt
+
+zip cobblestones.zip        cobblestones.pat        Thank\ You.txt
+zip hashing.zip             hashing.pat             Thank\ You.txt
